b64742a6c0dcc3e5c9ce7ba519bb0b1a6673e5f6
nadajinny <jinsun23007@gmail.com>
2026-03-16 16:31:14 +0900

    refactor: AssigneeTest에 test fixture 적용

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/AssigneeTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/AssigneeTest.kt
@@ -4,16 +4,22 @@ import kotlin.test.Test
 import kotlin.test.assertFailsWith
 
 class AssigneeTest {
+    private val blankText = " "
+    private val validText = "다이노"
     @Test
     fun `담당자는 빈 텍스트일 수 없다`() {
         assertFailsWith<IllegalArgumentException> {
-            Assignee(" ")
+            createAssignee(blankText)
         }
     }
 
     @Test
     fun `담당자는 텍스트 입력 시, 객체가 생성된다`() {
-        val assignee = Assignee("다이노")
-        assert(assignee.name == "다이노")
+        val assignee = createAssignee(validText)
+        assert(assignee.name == validText)
+    }
+
+    private fun createAssignee(name: String): Assignee {
+        return Assignee(name)
     }
 }
