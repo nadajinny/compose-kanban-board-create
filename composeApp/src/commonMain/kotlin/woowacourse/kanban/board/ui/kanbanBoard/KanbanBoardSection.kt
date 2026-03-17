84863aaf1959c464c89e9f84375d14e87fb89161
nadajinny <jinsun23007@gmail.com>
2026-03-17 17:53:13 +0900

    feat: taskCard 인자 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/kanbanBoard/KanbanBoardSection.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/kanbanBoard/KanbanBoardSection.kt
@@ -1,31 +1,24 @@
 package woowacourse.kanban.board.ui.kanbanBoard
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import woowacourse.kanban.board.model.TaskCard
+import woowacourse.kanban.board.ui.sample.TaskCardPreviewData
 
 @Preview(showBackground = true)
 @Composable
-private fun KanbanBoardSectionPreview() {
+private fun KanbanBoardSectionPreview(@PreviewParameter(TaskCardPreviewData::class) taskCard: TaskCard) {
     MaterialTheme{
-        KanbanBoardSection()
+        KanbanBoardSection(taskCard = taskCard)
     }
 }
 
 @Composable
-fun KanbanBoardSection() {
+fun KanbanBoardSection(taskCard: TaskCard) {
     Column {
         KanbanBoardHeaderSection()
         HorizontalDivider()
