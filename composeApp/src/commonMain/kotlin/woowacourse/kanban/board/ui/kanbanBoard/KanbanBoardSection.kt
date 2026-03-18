ebee051466055c7d87752599ddf47c0bcb4efc70
nadajinny <jinsun23007@gmail.com>
2026-03-18 14:28:11 +0900

    feat: 각 task마다 보는 것이 아니라 태스크 그룹 자체로 화면 표시되도록 설정

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/kanbanBoard/KanbanBoardSection.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/kanbanBoard/KanbanBoardSection.kt
@@ -1,25 +1,36 @@
 package woowacourse.kanban.board.ui.kanbanBoard
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import woowacourse.kanban.board.model.TaskCard
 import woowacourse.kanban.board.ui.sample.TaskCardPreviewData
 
 @Preview(showBackground = true)
 @Composable
-private fun KanbanBoardSectionPreview(@PreviewParameter(TaskCardPreviewData::class) taskCard: TaskCard) {
+private fun KanbanBoardSectionPreview() {
+    val taskCards = TaskCardPreviewData().values.toList()
     MaterialTheme{
-        KanbanBoardSection(taskCard = taskCard)
+        KanbanBoardSection(taskCards = taskCards)
     }
 }
 
 @Composable
-fun KanbanBoardSection(taskCard: TaskCard) {
-    Column {
+fun KanbanBoardSection(taskCards: List<TaskCard>) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
         KanbanBoardHeaderSection()
         HorizontalDivider()
         KanbanBoardBodySection()
