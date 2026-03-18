8df9a303d9d6043a86db2a0ebe923e9d4b6bfaa2
nadajinny <jinsun23007@gmail.com>
2026-03-18 14:42:06 +0900

    refactor: ktlint 포맷 정리

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/kanbanBoard/KanbanBoardBodySection.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/kanbanBoard/KanbanBoardBodySection.kt
@@ -4,11 +4,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import woowacourse.kanban.board.model.Status
 import woowacourse.kanban.board.model.TaskCard
 import woowacourse.kanban.board.ui.sample.TaskCardPreviewData
-import woowacourse.kanban.board.ui.taskCard.TaskCardSection
 
 @Preview(showBackground = true)
 @Composable
