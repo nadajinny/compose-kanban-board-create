16dab942195b04793d77ad8be506a79b4bb60cd2
nadajinny <jinsun23007@gmail.com>
2026-03-20 11:07:44 +0900

    feat: snack bar가 중앙에 오도록 설정

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/kanbanBoard/KanbanBoardSection.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/kanbanBoard/KanbanBoardSection.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarDuration
@@ -96,7 +97,6 @@ fun KanbanBoardSection(taskCards: List<TaskCard>) {
             hostState = snackBarHostState,
             modifier = Modifier
                 .align(Alignment.BottomCenter)
-                .fillMaxWidth(),
         )
     }
 }
