576fe98440ee5e51712a2b27d73afdfa9e79d032
nadajinny <jinsun23007@gmail.com>
2026-03-23 11:15:58 +0900

    feat: 스크롤 기능 추가(header는 이동하지 않아도 되므로 KanbanBoardBodySection에만 스크롤기능을 추가)

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/kanbanBoard/KanbanBoardBodySection.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/kanbanBoard/KanbanBoardBodySection.kt
@@ -1,8 +1,11 @@
 package woowacourse.kanban.board.ui.kanbanBoard
 
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import woowacourse.kanban.board.model.Status
 import woowacourse.kanban.board.model.TaskCard
@@ -19,7 +22,10 @@ private fun KanbanBoardBodySectionPreview() {
 
 @Composable
 fun KanbanBoardBodySection(taskCards: List<TaskCard>) {
-    Row {
+    Row(
+        modifier = Modifier
+            .horizontalScroll(rememberScrollState())
+    ) {
         Status.entries.forEach { status ->
             val taskByStatus = tasksByStatus(taskCards, status)
             KanbanColumnSection(status, taskByStatus)
